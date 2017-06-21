3583adb7d911ffa1e3586359d0c4e48eb974b31a
mike cohen <mcohen113@msn.com>
2017-06-21 15:14:22 -0400

    map init

--- js/main.js
+++ js/main.js
@@ -42,4 +42,20 @@ database.ref('reservations').on('child_added', function(snapshot) {
   reservationList.append(reservationTemplate);
 });
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7128, lng: -74.0059},
+    zoom: 8,
+    scrollwheel: false,
+    styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
+  });
+
+  // use Marker constructor to add a marker to map
+  var marker = new google.maps.Marker({
+    position: {lat: 40.7128, lng: -74.0059},
+    map: map,
+    title: 'Monks Café'
+  });
+}
+
 
