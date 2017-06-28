49705bb863172833ae6ffc7ce99b6dbf5ec4df1f
mike cohen <mcohen113@msn.com>
2017-06-28 16:05:37 -0400

    marker change

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function initMap() {
 
   // use Marker constructor to add a marker to map
   var marker = new google.maps.Marker({
-    position: {lat: 40.7128, lng: -74.0059},
+    position: {lat: 40.8054491, lng: -73.9654415},
     map: map,
     title: 'Monks Café'
   });
