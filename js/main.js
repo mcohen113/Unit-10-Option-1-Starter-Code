533474115e2bdecc27d6cc1b0fb9fcfe0753a1f0
mike cohen <mcohen113@msn.com>
2017-07-23 13:39:50 -0400

    href change to button for cancel, hover style

--- js/main.js
+++ js/main.js
@@ -43,8 +43,30 @@ $('.reservations').on('submit', (function(e) {
   };
 }));
 
+
+
+  //var currentTime = new Date().getHours();
+
 var d = new Date();
-document.getElementById("time").innerHTML = d.toString();
+document.getElementById("time").innerHTML = d.toLocaleString();
+
+var checkOpenStatus = function () {
+    var d = new Date();
+    var date = d.getHours();
+    var day = d.getDay();
+    if ((date > 9) && (date < 20) && (day != 0)) {
+        y = "<span style=\"color:#07ed11\">We're Open!</span>";
+    } else {
+        y = "<span style=\"color:#fc4b1c\">Sorry we're Closed.</span>";
+    }
+    document.getElementById("open-close").innerHTML = y;
+    setTimeout(checkOpenStatus,15000);
+};
+
+checkOpenStatus();
+
+
+
 
 
 // on initial load and addition of each reservation update the view
